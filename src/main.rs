57e79d4b640bfd8709a23e8557cb5dd43138854c
Pratik Gaikwad <pratikg@unfyd.com>
2026-01-29 00:08:33 +0530

    Csv reader CLI compleated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use csv;
+use clap::Parser;
+use std::{error::Error, path::PathBuf};
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args{
+    csvPath: PathBuf
+}
+fn read_from_csv(path: &PathBuf) -> Result<(), Box<dyn Error>> {
+let mut reader = csv::Reader::from_path(path)?;
+
+for result in reader.records() {
+  let record = result?;
+        println!("{:?}", record);
+}
+
+Ok(())
+}
+
 fn main() {
+    let args = Args::parse();
+   let path = args.csvPath;
+    if let Err(e) = read_from_csv(&path) {
+        eprintln!("{}",e) 
+    }
     println!("Hello, world!");
 }
